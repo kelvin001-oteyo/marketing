8cd8c65774815417c45f2940d3f69a8057f2c3ae
kelvin001-oteyo <oteyikelvin@gmail.com>
2026-09-10 21:12:55 +0300

    Add WhiteNoise middleware for static file handling

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
 
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
