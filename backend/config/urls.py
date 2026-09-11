9f4f3f9783bac5dc3d6b14d1970768eb04e80ad2
kelvin001-oteyo <oteyikelvin@gmail.com>
2026-09-11 12:33:51 +0300

    Refactor static URL pattern for media files

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -90,9 +90,8 @@ path(
 
 
 ]
-
 if settings.DEBUG:
-urlpatterns += static(
-settings.MEDIA_URL,
-document_root=settings.MEDIA_ROOT,
-)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
